39596cb372439e90585e6c763a3943d401cf5991
WalterWhite <doug.a.akridge@gmail.com>
2015-10-19 19:37:28 -0400

    add links to directory

--- application/handlers/dashboard/__init__.py
+++ application/handlers/dashboard/__init__.py
@@ -8,12 +8,20 @@ from flask import redirect
 from flask import send_file
 from flask import render_template
 
+import db
 import application
 import application.tools as tools
 
 @application.app.route("/dashboard/", methods=["GET"])
-@tools.dbauthenticated
-def dashboard( user ):
-    
-    return render_template("dashboard/dashboard.html",
-    	user=user)
+@application.app.route("/dashboard/<userid>/", methods=["GET"])
+@tools.authenticated
+def dashboard( user, userid ):
+
+	if userid == None:
+		userid = user["userid"]
+
+	profile_user = db.user.get( userid )
+	
+	return render_template("dashboard/dashboard.html",
+		user=user,
+		profile_user=profile_user)
