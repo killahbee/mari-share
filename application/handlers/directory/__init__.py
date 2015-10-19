ff3fe4b604df92d1f1fb1f4cc9e461f1465701bd
WalterWhite <doug.a.akridge@gmail.com>
2015-10-19 19:30:13 -0400

    add directory

--- application/handlers/directory/__init__.py
+++ application/handlers/directory/__init__.py
@@ -7,9 +7,15 @@ from flask import url_for
 from flask import send_file
 from flask import render_template
 
+import db
 import application
+import application.tools as tools
 
 @application.app.route("/directory/", methods=["GET"])
-def directory():
-    
-    return render_template("directory/directory.html")
+@tools.authenticated
+def directory( user ):
+
+	users = db.user.get()
+	
+	return render_template("directory/directory.html",
+		users=users)
--- db/user/__init__.py
+++ db/user/__init__.py
@@ -1,21 +1,27 @@
 import psycopg2.extras
 import db
 
-def get( userid ):
+def get( userid=None, cols="*" ):
 
 	try:
 		conn = db.get_db()
-
-		sql_query = "SELECT * FROM users WHERE userid = %s;"
-		sql_data = (userid, )
-
 		cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-		cur.execute( sql_query, sql_data )
-		user = cur.fetchone()
+
+		if userid:
+			sql_query = "SELECT * FROM users WHERE userid = %s;"
+			sql_data = (userid, )
+			cur.execute( sql_query, sql_data )
+			payload = cur.fetchone()
+
+		else:
+			sql_query = "SELECT * FROM users ORDER BY username ASC;"
+			cur.execute( sql_query )
+			payload = cur.fetchall()
+
 		cur.close()
 
 	except Exception, e:
 		print e
-		user = {}
+		payload = {}
 
-	return user
+	return payload
